ec9ba8b89784bf68d7b5aae8ce655a08eec429f9
Edmond <root@localhost.localdomain>
2017-05-03 13:14:28 -0400

    Another fix

--- src/test/scala/com/akkademy/AkkademyDbSpec.scala
+++ src/test/scala/com/akkademy/AkkademyDbSpec.scala
@@ -17,9 +17,9 @@ class AkkademyDbSpec extends FunSpecLike with Matchers with BeforeAndAfterEach
         it ("should place key/value into map")
         {
           val actorRef = TestActorRef(new AkkademyDB)
-          actorRef ! SetRequest("key1","value1")
+          actorRef ! SetRequest("key2","value2")
           val akkademyDb = actorRef.underlyingActor
-          akkademyDb.map.get("key1") should equal(Some("value1"))
+          akkademyDb.map.get("key2") should equal(Some("value2"))
         }
       }
   }
